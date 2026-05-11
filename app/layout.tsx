0d903d3a27145db24afa2c0d8569f51c8ae324b6
Tomorsukh-Munkhtur <tomorsukh.job@gmail.com>
2026-05-11 15:48:32 +0800

    layout.tsx өөрчлөгдсөн

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// @ts-ignore: CSS module declaration missing
+// @ts-expect-error: CSS module declaration missing
 import "./globals.css";
 
 const geistSans = Geist({
